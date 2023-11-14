ae291fb15acb1a0f62a756297cc8d03b5c9eec9f
Truong Phan <phanlethanhtruong@gmail.com>
2023-11-14 08:21:20 +0700

    Add newsletter

---
+++ java.js
@@ -0,0 +1,9 @@
+const form = document.querySelector("form");
+const card_1 = document.querySelector(".container");
+const card_2 = document.querySelector(".card");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    card_1.classList.add("hide");
+    card_2.classList.remove("hide");
+});
