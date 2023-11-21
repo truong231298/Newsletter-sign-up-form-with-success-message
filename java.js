86a2a5c1eaf3eb246e0dcef373ec755aafd709ef
Truong Phan <phanlethanhtruong@gmail.com>
2023-11-21 09:16:05 +0700

    change third

--- java.js
+++ java.js
@@ -2,7 +2,7 @@ const form = document.querySelector("form");
 const card_1 = document.querySelector(".container");
 const card_2 = document.querySelector(".card");
 
-form.addEventListener("submit", (e) => {
+form.addEventListener('submit', (e) => {
     e.preventDefault();
     card_1.classList.add("hide");
     card_2.classList.remove("hide");
